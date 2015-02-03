405563e9dd306d5d8b9f102b4a51f1ba3afefb5c
John W <wislon@gmail.com>
2015-02-04 09:39:39 +1000

    add akacache.sqlite3 and reactiveui packages (which added all their dependencies)

--- src/AkavacheTest/Resources/Resource.Designer.cs
+++ src/AkavacheTest/Resources/Resource.Designer.cs
@@ -26,6 +26,7 @@ namespace AkavacheTest
 		
 		public static void UpdateIdValues()
 		{
+			global::Splat.Resource.String.library_name = global::AkavacheTest.Resource.String.library_name;
 		}
 		
 		public partial class Attribute
@@ -92,11 +93,14 @@ namespace AkavacheTest
 		public partial class String
 		{
 			
+			// aapt resource value: 0x7f040002
+			public const int ApplicationName = 2130968578;
+			
 			// aapt resource value: 0x7f040001
-			public const int ApplicationName = 2130968577;
+			public const int Hello = 2130968577;
 			
 			// aapt resource value: 0x7f040000
-			public const int Hello = 2130968576;
+			public const int library_name = 2130968576;
 			
 			static String()
 			{

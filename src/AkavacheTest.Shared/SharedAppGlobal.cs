ca91a87cb4aedb4e10d05ef902cb647a95ee1216
John W <wislon@gmail.com>
2015-02-04 10:01:31 +1000

    added shared pcl lib and akavache/rxui packages

---
+++ src/AkavacheTest.Shared/SharedAppGlobal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Akavache;
+
+namespace AkavacheTest.Shared
+{
+    public static class SharedAppGlobal
+    {
+        static SharedAppGlobal()
+        {
+            BlobCache.ApplicationName = "AkavacheTest";
+            BlobCache.EnsureInitialized();
+        }
+    }
+}

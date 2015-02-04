2bada1e80eebeafcd215da0a285b500267e5e669
John W <wislon@gmail.com>
2015-02-05 06:51:19 +1000

    provide database file path for sqlite cache db files and instantiate IBlobCache

--- src/AkavacheTest.Shared/SharedAppGlobal.cs
+++ src/AkavacheTest.Shared/SharedAppGlobal.cs
@@ -1,17 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Akavache;
+﻿using Akavache;
+using Akavache.Sqlite3;
 
 namespace AkavacheTest.Shared
 {
     public static class SharedAppGlobal
     {
+        public static bool Initialised { get; private set; }
+
+        public static IBlobCache NormalCache { get; private set; }
+
         static SharedAppGlobal()
         {
-            BlobCache.ApplicationName = "AkavacheTest";
-            BlobCache.EnsureInitialized();
+        }
+
+        public static void Init(string pathToCacheDbFile)
+        {
+            if (!Initialised)
+            {
+                BlobCache.ApplicationName = "AkavacheTest";
+                BlobCache.EnsureInitialized();
+
+                // correct encryption provider and task scheduler are taken care of by Akavache if those params are left empty
+                // EncryptedCache = new SQLiteEncryptedBlobCache(pathToCacheDbFile, encryptionProvider: null, scheduler: null); 
+
+                // correct task scheduler is provided by Akavache if the 'scheduler' parameter is left empty
+                NormalCache = new SQLitePersistentBlobCache(pathToCacheDbFile, scheduler: null); 
+
+                Initialised = true;
+            }
         }
     }
 }
--- src/AkavacheTest/Views/BaseView.cs
+++ src/AkavacheTest/Views/BaseView.cs
@@ -1,17 +1,22 @@
 ﻿using System;
+using AkavacheTest.Shared;
 using Android.OS;
 using ReactiveUI;
 
 namespace AkavacheTest.Views
 {
-	public class BaseView<T> : ReactiveActivity<T> where T : ReactiveObject
-	{
-	
-		protected override void OnCreate(Bundle savedInstanceState)
-		{
-			base.OnCreate(savedInstanceState);
-			ViewModel = (T)Activator.CreateInstance (typeof (T));
-		}
-	}
+    public class BaseView<T> : ReactiveActivity<T> where T : ReactiveObject
+    {
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            string pathToCacheDbFile = System.IO.Path.Combine(GetExternalFilesDir(null).AbsolutePath, "cache.db");
+            SharedAppGlobal.Init(pathToCacheDbFile);
+
+            ViewModel = (T)Activator.CreateInstance(typeof(T));
+        }
+    }
 }
 

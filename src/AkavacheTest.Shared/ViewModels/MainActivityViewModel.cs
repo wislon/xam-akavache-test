5f89f3deea7ce771bcb45f97ea90fd3369dc91fe
John W <wislon@gmail.com>
2015-02-04 10:13:56 +1000

    add a dumb mainactivityviewmodel for reading/writing to cache

---
+++ src/AkavacheTest.Shared/ViewModels/MainActivityViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using Akavache;
+using ReactiveUI;
+using Splat;
+
+namespace AkavacheTest.Shared.ViewModels
+{
+    public class MainActivityViewModel : ReactiveObject
+    {
+        private string _textToSave;
+        private string _textFromCache;
+
+        public string TextToSave
+        {
+            get { return _textToSave; }
+            set { this.RaiseAndSetIfChanged(ref _textToSave, value); }
+        }
+
+        public string TextFromCache
+        {
+            get { return _textFromCache; }
+            set { this.RaiseAndSetIfChanged(ref _textFromCache, value); }
+        }
+
+        public ReactiveCommand<string> WriteStringToCache { get; set; }
+        public ReactiveCommand<string> ReadStringFromCache { get; set; }
+
+        public MainActivityViewModel()
+        {
+            var canStore = this.WhenAnyValue(vm => vm.TextToSave, s => !string.IsNullOrWhiteSpace(s));
+            WriteStringToCache = ReactiveCommand.CreateAsyncTask(canStore, async _ => await WriteStringToCacheImpl());
+
+            WriteStringToCache.Subscribe(returnedString => TextFromCache = returnedString);
+
+            WriteStringToCache.ThrownExceptions.Subscribe(
+                exception => this.Log().ErrorException("There was an error reading/writing to cache", exception));
+
+
+            ReadStringFromCache = ReactiveCommand.CreateAsyncTask(canStore, async _ => await ReadStringFromCacheImpl());
+            ReadStringFromCache.Subscribe(returnedString => TextFromCache = returnedString);
+
+            ReadStringFromCache.ThrownExceptions.Subscribe(
+                exception => this.Log().ErrorException("There was an error reading from the cache", exception));
+
+        }
+
+        private async Task<string> WriteStringToCacheImpl()
+        {
+            string text = _textToSave;
+            await BlobCache.UserAccount.InsertObject("text", text);
+            return await BlobCache.UserAccount.GetObject<string>("text");
+        }
+
+        private async Task<string> ReadStringFromCacheImpl()
+        {
+            return await BlobCache.UserAccount.GetObject<string>("text");
+        }
+
+    }
+}

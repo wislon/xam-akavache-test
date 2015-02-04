5f77643b31dd8ef32f094561e6ee77d75943fe0e
John W <wislon@gmail.com>
2015-02-04 10:43:09 +1000

    reference and use the dumbest read/write viewmodel you ever saw

--- src/AkavacheTest/MainActivity.cs
+++ src/AkavacheTest/MainActivity.cs
@@ -1,30 +1,41 @@
-﻿using System;
+﻿using AkavacheTest.Shared.ViewModels;
+using AkavacheTest.Views;
 using Android.App;
-using Android.Content;
-using Android.Runtime;
-using Android.Views;
 using Android.Widget;
 using Android.OS;
+using ReactiveUI;
 
 namespace AkavacheTest
 {
     [Activity(Label = "AkavacheTest", MainLauncher = true, Icon = "@drawable/icon")]
-    public class MainActivity : Activity
+    public class MainActivity : BaseView<MainActivityViewModel>
     {
-        int count = 1;
+        private EditText _edTextToSave;
+        private TextView _txtFromCache;
+        private Button _btnWriteToCache;
+        private Button _btnReadFromCache;
 
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.Main);
+            SetContentView(Resource.Layout.MainActivity);
 
-            // Get our button from the layout resource,
-            // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.MyButton);
+            FindAndBindUiElements();
+        }
 
-            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+        private void FindAndBindUiElements()
+        {
+            _edTextToSave = FindViewById<EditText>(Resource.Id.edTextToSave);
+            _txtFromCache = FindViewById<TextView>(Resource.Id.txtFromCache);
+            _btnWriteToCache = FindViewById<Button>(Resource.Id.btnWriteToCache);
+            _btnReadFromCache = FindViewById<Button>(Resource.Id.btnReadFromCache);
+
+            this.Bind(ViewModel, vm => vm.TextToSave, v => v._edTextToSave.Text);
+            this.OneWayBind(ViewModel, vm => vm.TextFromCache, v => v._txtFromCache.Text);
+            this.BindCommand(ViewModel, vm => vm.WriteStringToCache, v => v._btnWriteToCache);
+            this.BindCommand(ViewModel, vm => vm.ReadStringFromCache, v => v._btnReadFromCache);
         }
     }
 }

ff411da24368ee326172dfbb3fc484136b82b723
John W <wislon@gmail.com>
2015-02-04 10:41:51 +1000

    add a BaseView type for instantiating viewmodels

---
+++ src/AkavacheTest/Views/BaseView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Android.OS;
+using ReactiveUI;
+
+namespace AkavacheTest.Views
+{
+	public class BaseView<T> : ReactiveActivity<T> where T : ReactiveObject
+	{
+	
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+			ViewModel = (T)Activator.CreateInstance (typeof (T));
+		}
+	}
+}
+
